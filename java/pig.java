7e871c182714234de926a5e30f1708a6d3ba05a4
xugaoshi <37128863+xugaoshi@users.noreply.github.com>
2019-08-28 09:56:28 +0800

    Update pig.java

--- java/pig.java
+++ java/pig.java
@@ -2,6 +2,7 @@ public class pig {
     public static void main(String[] args) {
         int s=5,m=6,w=5,p=9;
         System.out.println(s+m);
+        System.out.println(w+p);
         //测试idea的pull
     }
 }
